1448e9100104077ff65239cb5e46d9329bb12dc3
SreedharAdapala <sreedhar.adapala@perennialsys.com>
2024-01-11 16:44:20 +0530

    Second sample commit

---
+++ UnitTestExample/LoginModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginModel.swift
+//  UnitTestExample
+//
+//  Created by Perennials on 11/01/24.
+//
+
+import Foundation
+
+
+struct LoginCredentials {
+    var username: String = ""
+    var password: String = ""
+}
